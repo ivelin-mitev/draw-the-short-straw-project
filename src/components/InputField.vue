6a15e8db1d33d2608735bd9da5b04903c25d716c
Ivelin Mitev <ivelin.mitev@mentormate.com>
2022-10-30 21:37:56 +0200

    Add input fields on homepage

---
+++ src/components/InputField.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h2 v-if="title">{{ title }}</h2>
+        <input :style="{width: widthValue + 'px' }" v-model="text" :placeholder="placeholderValue" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InputField',
+    props: {
+        title: String,
+        placeholderValue: {
+            type: String,
+            default: ''
+        },
+        widthValue: {
+            type: Number,
+            default: 200
+        }
+    },
+
+    data() {
+        return {
+            text: ''
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+        console.log(this.widthValue)
+    }
+}
+</script>
+
+<style scoped>
+div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,18 +1,43 @@
-<script setup>
-</script>
-
 <template>
   <header>
-    <img class="logo" src="src\assets\Draw-the-Short-Straw-Upscaled-x4-no-bg.png"/>
+    <img class="logo" src="src\assets\Draw-the-Short-Straw-Upscaled-x4-no-bg.png" />
     <h1 class="title">
       Draw the Short Straw
     </h1>
+
   </header>
+
+  <InputField title='Write down your question' placeholderValue="Who's buying beer tonight?" :widthValue="`${400}`"/>
+  <InputField title='Enter your name' placeholderValue="Name" :widthValue="`${300}`"/>
 </template>
+
+<script>
+import InputField from '@/components/InputField.vue';
+
+export default {
+  components: {
+    InputField
+  },
+
+  data() {
+    return {
+    }
+  },
+
+  methods: {
+
+  },
+  mounted() {
+
+  }
+}
+</script>
 
 <style scoped>
 header {
   display: flex;
   align-items: center;
+
+  margin: 42px 0px;
 }
 </style>
