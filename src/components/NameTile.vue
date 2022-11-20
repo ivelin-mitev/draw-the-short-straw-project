0f7b285b65f3479d21545c359f4ae9922a876b7b
Ivelin Mitev <ivelin.mitev@mentormate.com>
2022-11-20 22:13:42 +0200

    Add tiles with names on the Waiting Page

---
+++ src/components/NameTile.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="nameTile">{{name}}</div>
+</template>
+<script>
+export default {
+    props: {
+        name: String
+    }
+}
+</script>
--- src/components/WaitingPage.vue
+++ src/components/WaitingPage.vue
@@ -1,29 +1,84 @@
 <template>
     <header>
-        <h1 class="title">
-            Waiting Page
+        <h1 class="title">Players:
         </h1>
-        <button @click="sendTheMsg">Click</button>
     </header>
+    <div class="nameTiles">
+        <NameTile name="Ivo" />
+        <NameTile name="Pivo" />
+        <NameTile name="Divo" />
+        <NameTile name="Krivo" />
+    </div>
+    <ButtonVue text="START">
+        <IconPlay />
+    </ButtonVue>
+    <ButtonVue @click.native="copyURL" text="Copy URL link" :showBadge="showBadge">
+        <IconLinkChain width="22" height="22" />
+    </ButtonVue>
 </template>
 
 <script>
-import { Socket } from 'socket.io-client';
-import VueSocketIO from 'vue-socket.io'
+import ButtonVue from './ButtonVue.vue';
+import IconPlay from './icons/IconPlay.vue';
+import IconLinkChain from './icons/IconLinkChain.vue'
+import { mapState } from 'vuex'
+import NameTile from './NameTile.vue';
 
 export default {
+    components: {
+        NameTile,
+        ButtonVue,
+        IconPlay,
+        IconLinkChain
+    },
     sockets: {
-        connect: function () {
-            console.log('socket connected')
-        },
-        customEmit: function (data) {
-            console.log('this method was fired by the socket server. eg: io.emit("customEmit", data)')
+        newPlayerId: function (data) {
+            console.log(this.players);
+    },
+  },
+    data() {
+        return {
+            showBadge: false,
         }
+    },
+    computed: {
+        ...mapState({
+            url: state => state.url,
+            players: state => state.players
+        })
     },
     methods: {
         sendTheMsg() {
-            this.$socket.client.emit('SEND_MESSAGE', 'ZDR')
+            this.$socket.client.emit('SEND_MESSAGE', 'ZDR');
+        },
+        async copyURL() {
+            try {
+                await navigator.clipboard.writeText(this.url);
+                this.toggleBadge();
+            } catch (err) {
+                console.log('Cannot copy', err);
+            }
+        },
+        toggleBadge() {
+            this.showBadge = true;
+            setTimeout(() => {
+                this.showBadge = false;
+            }, 1000);
         }
+    },
+    mounted() {
+        console.log(this.url + 'ei go na')
     }
 }
 </script>
+
+<style scoped>
+header {
+    align-self: center;
+    margin: 84px 0px 42px;
+}
+
+.nameTiles {
+    align-items: center;
+}
+</style>
