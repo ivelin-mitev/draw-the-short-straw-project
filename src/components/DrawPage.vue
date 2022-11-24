7c605e0f4eca18bed5de367478289654de36c4b0
Ivelin Mitev <ivelin.mitev@mentormate.com>
2022-11-24 11:15:27 +0100

    Created DrawPage boilerplate

---
+++ src/components/DrawPage.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="wrapper">
+        <header>
+            <h1 class="title">Who is going to buy beer tonight?
+            </h1>
+        </header>
+        <div class="nameTiles">
+            <NameTile name="Name" />
+            <NameTile name="Name" />
+            <NameTile name="Name" />
+            <NameTile name="Name" />
+        </div>
+        <div id="straws">
+            <img class="straw" src="../assets/straw 1.png" />
+            <img class="straw" src="../assets/straw 2.png" />
+            <img class="straw" src="../assets/straw 3.png" />
+            <img class="straw" src="../assets/straw 4.png" />
+            <img class="straw" src="../assets/straw 5.png" />
+        </div>
+    </div>
+</template>
+
+<script>
+import NameTile from './NameTile.vue';
+
+export default {
+    components: {
+        NameTile
+    }
+}
+</script>
+
+<style scoped>
+.wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+header {
+    align-self: center;
+    margin: 84px 0px 42px;
+}
+.nameTiles {
+    align-items: center;
+}
+#straws{
+    position: fixed;
+    display: flex;
+    gap:50px;
+    bottom: 0%;
+}
+</style>
